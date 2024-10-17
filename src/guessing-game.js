2bd6dfffbe5788d6b6faeffedf56ad4f78f06a4e
MarinaYur <marina.yurkev@gmail.com>
2024-10-17 15:02:15 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.random() * (this.max - this.min) + this.min;
+        return this.guessNumber;
     }
 
     lower() {
-
+        return this.min = this.guessNumber;
     }
 
     greater() {
-
+        return this.max = this.guessNumber;
     }
 }
 
